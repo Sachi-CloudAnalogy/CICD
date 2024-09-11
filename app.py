359917881656854ad74ce738531cbad786a0d204
Sachi Gaur <sachi.gaur@cloudanalogy.com>
2024-09-11 13:02:27 +0530

    new changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello World !!"
+    return "<h1>Hello World !! This Page is using CI/CD pipeline...</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
